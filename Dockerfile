6f777fed82893eff87668d0d995ef118bf2b297d
julia <julianschlicker@t-online.de>
2022-02-16 20:57:41 +0100

    Files has been added.

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Stage 1
+# Installing base image
+FROM python:3 
+# Getting image updates
+RUN apt-get update
+
+# Stage 2
+# Setting working directory
+WORKDIR /usr/local/app
+# Adding source code to WORKDIR
+COPY ./ /usr/local/app/
+
+#Stage 3
+# Installing necessary libraries
+RUN pip install pygame
+
+#Stage 4
+# Running the game
+# CMD [ "python3", "/usr/local/app/main.py" ]
+                                                     
+
+
+
+
---
+++ src/code/main.py
@@ -0,0 +1,32 @@
+from turtle import width
+import pygame, sys
+from src.code.settings import *
+from debug import debug
+from src.code.level import Level
+
+class Game:
+    def __init__(self) :
+        
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('python-game')
+        self.clock = pygame.time.Clock()
+
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
